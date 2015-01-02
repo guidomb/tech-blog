a8dd96c57f1d395eb6313a7cf482c2a618aee881
Guido Marucci Blas <guidomb@gmail.com>
2015-01-01 23:29:25 -0300

    Setups for github deploy

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/tech-blog/"
+http_images_path = "/tech-blog/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/tech-blog/fonts"
+css_dir = "public/tech-blog/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
